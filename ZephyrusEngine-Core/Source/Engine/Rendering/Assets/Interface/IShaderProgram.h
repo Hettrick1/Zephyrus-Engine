703635f227209c4a50a99244182c24daf2610f41
Evann Jacquot <evann.jacquot@artfx.fr>
2025-10-30 12:03:36 +0100

    [add] IShaderProgram

---
+++ ZephyrusEngine-Core/Source/Engine/Rendering/Assets/Interface/IShaderProgram.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Matrix4DRow.h"
+#include "Matrix4D.h"
+#include "Vector2D.h"
+#include "Vector3D.h"
+#include "Vector4D.h"
+
+#include "Interface/IShader.h"
+
+#include <vector>
+
+namespace Zephyrus::Render 
+{
+	class IShaderProgram
+	{
+	public:
+		virtual ~IShaderProgram() = default;
+		// Unloads the shader program from GPU memory
+		virtual void Unload() = 0;
+		
+		// Composes the shader program from a list of shaders
+		virtual void Compose(std::vector<IShader*>& pShaders) = 0;
+		 
+		virtual inline unsigned int GetID() const = 0;
+		virtual inline uint8_t GetType() const = 0;
+ 
+		virtual void Use() = 0;
+		
+		// Sets a float uniform variable in the shader
+		virtual void setFloat(const char* name, float value) = 0;
+		// Sets an integer uniform variable in the shader
+		virtual void setInteger(const char* name, int value) = 0;
+		// Sets a vec2 uniform variable in the shader (from two floats)
+		virtual void setVector2f(const char* name, float x, float y) = 0;
+		// Sets a vec2 uniform variable in the shader (from a Vector2D)
+		virtual void setVector2f(const char* name, const Vector2D& value) = 0;
+		// Sets a vec3 uniform variable in the shader (from three floats)
+		virtual void setVector3f(const char* name, float x, float y, float z) = 0;
+		// Sets a vec3 uniform variable in the shader (from a Vector3D)
+		virtual void setVector3f(const char* name, const Vector3D& value) = 0;
+		// Sets a vec4 uniform variable in the shader (from four floats)
+		virtual void setVector4f(const char* name, float x, float y, float z, float w) = 0;
+		// Sets a vec4 uniform variable in the shader (from a Vector4D)
+		virtual void setVector4f(const char* name, const Vector4D& value) = 0;
+		// Sets a mat4 uniform variable in the shader (column-major)
+		virtual void setMatrix4(const char* name, const Matrix4D& matrix) = 0;
+		// Sets a mat4 uniform variable in the shader (row-major)
+		virtual void setMatrix4Row(const char* name, const Matrix4DRow& matrix) = 0;
+	}; 
+}
--- ZephyrusEngine-Core/Source/Engine/Rendering/Assets/Shader/ShaderProgram.h
+++ ZephyrusEngine-Core/Source/Engine/Rendering/Assets/Shader/ShaderProgram.h
@@ -2,61 +2,50 @@
 
 #include "glew.h"
 
-#include "Interface/IShader.h"
-
-#include "Matrix4DRow.h"
-#include "Matrix4D.h"
-#include "Vector2D.h"
-#include "Vector3D.h"
-#include "Vector4D.h"
-#include "ShaderProgramType.h"
-
-#include <vector>
+#include "Interface/IShaderProgram.h"
 
 /**
  * @class ShaderProgram
  * @brief Manages an OpenGL shader program, allowing composition, usage, and uniform variable setting.
  */
 namespace Zephyrus::Render {
-	class ShaderProgram
+	class ShaderProgram : IShaderProgram
 	{
 	private:
-		unsigned int mId;
-		uint8_t mShaderProgramType;
+		unsigned int mId{ 0 };
+		uint8_t mShaderProgramType{ 0 };
 
 	public:
-		ShaderProgram();
-		~ShaderProgram() = default;
-
+		ShaderProgram() = default;
 		// Unloads the shader program from GPU memory
-		void Unload();
+		void Unload() override;
 
 		// Composes the shader program from a list of shaders
-		void Compose(std::vector<IShader*>& pShaders);
+		void Compose(std::vector<IShader*>& pShaders) override;
 
-		inline unsigned int GetID() const { return mId; }
-		inline uint8_t GetType() const { return mShaderProgramType; }
+		inline unsigned int GetID() const override { return mId; }
+		inline uint8_t GetType() const override { return mShaderProgramType; }
 		void Use();
 
 		// Sets a float uniform variable in the shader
-		void setFloat(const GLchar* name, GLfloat value);
+		void setFloat(const GLchar* name, GLfloat value) override;
 		// Sets an integer uniform variable in the shader
-		void setInteger(const GLchar* name, GLint value);
+		void setInteger(const GLchar* name, GLint value) override;
 		// Sets a vec2 uniform variable in the shader (from two floats)
-		void setVector2f(const GLchar* name, GLfloat x, GLfloat y);
+		void setVector2f(const GLchar* name, GLfloat x, GLfloat y) override;
 		// Sets a vec2 uniform variable in the shader (from a Vector2D)
-		void setVector2f(const GLchar* name, const Vector2D& value);
+		void setVector2f(const GLchar* name, const Vector2D& value) override;
 		// Sets a vec3 uniform variable in the shader (from three floats)
-		void setVector3f(const GLchar* name, GLfloat x, GLfloat y, GLfloat z);
+		void setVector3f(const GLchar* name, GLfloat x, GLfloat y, GLfloat z) override;
 		// Sets a vec3 uniform variable in the shader (from a Vector3D)
-		void setVector3f(const GLchar* name, const Vector3D& value);
+		void setVector3f(const GLchar* name, const Vector3D& value) override;
 		// Sets a vec4 uniform variable in the shader (from four floats)
-		void setVector4f(const GLchar* name, GLfloat x, GLfloat y, GLfloat z, GLfloat w);
+		void setVector4f(const GLchar* name, GLfloat x, GLfloat y, GLfloat z, GLfloat w) override;
 		// Sets a vec4 uniform variable in the shader (from a Vector4D)
-		void setVector4f(const GLchar* name, const Vector4D& value);
+		void setVector4f(const GLchar* name, const Vector4D& value) override;
 		// Sets a mat4 uniform variable in the shader (column-major)
-		void setMatrix4(const GLchar* name, const Matrix4D& matrix);
+		void setMatrix4(const GLchar* name, const Matrix4D& matrix) override;
 		// Sets a mat4 uniform variable in the shader (row-major)
-		void setMatrix4Row(const GLchar* name, const Matrix4DRow& matrix);
+		void setMatrix4Row(const GLchar* name, const Matrix4DRow& matrix) override;
 	};
 }
